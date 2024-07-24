68f2f61bb056322b834494d3b12e7a64146de444
app-c <william@app-com.digital>
2024-07-24 11:46:32 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@
 import { format } from "date-fns";
 import { addDoc, collection, getDocs } from 'firebase/firestore';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import React, { ChangeEvent, useEffect } from "react";
+import React, { ChangeEvent } from "react";
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { useQuery } from "react-query";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -39,7 +40,7 @@ interface I {
 export default function Home() {
   const key = 'comprovant'
   const [file, setFile] = React.useState<any>(null)
-  const [document, setDocument] = React.useState<I[]>([])
+  const [value, setValue] = React.useState('')
 
   const colllectionName = collection(database, key)
 
@@ -52,18 +53,31 @@ export default function Home() {
     [],
   )
 
-  useEffect(() => {
-    getDocs(colllectionName).then(h => {
+  async function getDocumentos() {
+    const rs = await getDocs(colllectionName).then(h => {
       const dc = h.docs.map(h => h.data() as I)
 
-      setDocument(dc)
+      return dc as I[]
     })
+
+
+    return rs
+  }
+
+  // React.useEffect(() => {
+  //   getDocumentos()
+  // }, [])
+
+  const { data: documento = [] } = useQuery({
+    queryKey: ['docs'],
+    queryFn: getDocumentos,
   })
+
 
   async function addNota() {
     try {
 
-      const imageRef = ref(storage, `comprovante/${document.length === 0 ? 1 : document.length}`)
+      const imageRef = ref(storage, `comprovante/${documento.length === 0 ? 1 : documento.length}`)
 
       let imageUrl = null
       await uploadBytes(imageRef, file).then(async (h) => {
@@ -73,9 +87,9 @@ export default function Home() {
 
 
       await addDoc(colllectionName, {
-        parcela: document.length === 0 ? 1 : document.length,
+        parcela: documento.length + 1,
         data: format(new Date(), 'dd/MM/ HH:mm'),
-        valor: 1333.38,
+        valor: Number(value),
         comprovante: imageUrl
       })
 
@@ -88,7 +102,7 @@ export default function Home() {
     }
   }
 
-  const currentValor = document.reduce((acc, i) => acc + i.valor, 0)
+  const currentValor = documento.reduce((acc, i) => acc + i.valor, 0)
 
   function converValor(valor: number) {
     const formated = valor.toLocaleString('pt-BR', {
@@ -99,7 +113,9 @@ export default function Home() {
     return formated
   }
 
+
   return (
+
     <div className="flex flex-col p-4 ">
       <div className="flex flex-col items-center justify-center w-fulls gap-3" >
         <input
@@ -108,13 +124,15 @@ export default function Home() {
           placeholder=""
           name="plant"
         />
+
+        <input className="text-gray-900" type="text" placeholder="valor transferencia" onChange={h => setValue(h.currentTarget.value)} value={value} />
         <button className="bg-green-700 p-2 px-4 rounded-md" onClick={addNota} >Salvar documento</button>
       </div>
       <p className="m-4 text-2 text-center">Manutenção do veículo Honda city - 2010</p>
       <div className="p-10 gap-2  bg-slate-900 items-center flex flex-col" >
         <div className="flex flex-col w-full" >
           <h1 className="font-black text-[6.5svw] text-orange-500">Meta de pagamento: </h1>
-          <h1>R$ 13.333,38</h1>
+          <h1>R$ 13.480,47</h1>
 
         </div>
 
@@ -139,8 +157,8 @@ export default function Home() {
           </tr>
         </thead>
         <tbody className="bg-gray-800 divide-y divide-gray-200">
-          {document.map(h => (
-            <tr key={h.parcela}>
+          {documento.map((h, i) => (
+            <tr key={i}>
               <td className="px-2 py-4 text-[14px] whitespace-nowrap">
                 <h4 className="text-[10px]" >{h.parcela}</h4>
 
